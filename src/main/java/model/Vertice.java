3a72b23161450d3fb9f99d2ebdeb43ba7eb56255
Marcos Vinicius Almeida Guimaraes <mvaguimaraes@gmail.com>
2020-05-09 12:14:39 -0300

    Adicionando objetos Encomenda, Grafo e Vertice

---
+++ src/main/java/model/Vertice.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertice implements Comparable<Vertice> {
+    private final String nome;
+    private int distancia = Integer.MAX_VALUE;
+    private Vertice anterior = null;
+    private final Map<Vertice, Integer> vizinhos = new HashMap<>();
+
+    public Vertice(String nome) {
+        this.nome = nome;
+    }
+
+    public void addVizinho(Vertice vizinho, int distancia) {
+        vizinhos.put(vizinho, distancia);
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public Vertice getAnterior() {
+        return anterior;
+    }
+
+    void setAnterior(Vertice anterior) {
+        this.anterior = anterior;
+    }
+
+    Map<Vertice, Integer> getVizinhos() {
+        return vizinhos;
+    }
+
+    public int compareTo(Vertice other) {
+        if (distancia == other.distancia)
+            return nome.compareTo(other.nome);
+
+        return Integer.compare(distancia, other.distancia);
+    }
+}
