9ce0f9608f977679624b3df9a2937360aae6af4c
Marcos Vinicius Almeida Guimaraes <mvaguimaraes@gmail.com>
2020-05-09 13:14:37 -0300

    Revertendo modificação desnecessária em Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
         //Processando o arquivo de trechos
         Map<String, Vertice> trechos = FileUtils.lerArquivoTrechos("trechos");
         //Processando o arquivo de encomendas.txt
-        List<Encomenda> encomendas = FileUtils.lerArquivoEncomendas("encomendas.txt");
+        List<Encomenda> encomendas = FileUtils.lerArquivoEncomendas("encomendas");
         //Gerando o arquivo de rotas
         FileUtils.processarEncomendas(trechos, encomendas, "rotas");
     }
