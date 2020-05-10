338389f596c5e62aa7116b092fa8959131e813a7
Marcos Vinicius Almeida Guimaraes <mvaguimaraes@gmail.com>
2020-05-10 09:50:38 -0300

    Checagem de parâmetros nulos

--- src/main/java/controller/ControllerDeEncomendas.java
+++ src/main/java/controller/ControllerDeEncomendas.java
@@ -19,63 +19,70 @@ public class ControllerDeEncomendas {
      */
     public void processarEncomendas(List<Encomenda> encomendas, String nomeArquivoRotas) {
 
-        for (Encomenda encomenda : encomendas) {
-            menorCaminhoDijkstra(encomenda.getOrigem());
-            if (!trechos.containsKey(encomenda.getOrigem())) {
-                System.out.println(String.format("MapaDeTrechos não contém o vértice de origem: \"%s\"\n", encomenda.getOrigem()));
-            } else {
-                FileUtils.escreverArquivoRotas(trechos.get(encomenda.getDestino()), nomeArquivoRotas);
+        if (!Objects.isNull(encomendas) && !Objects.isNull(nomeArquivoRotas)) {
+            for (Encomenda encomenda : encomendas) {
+                menorCaminhoDijkstra(encomenda.getOrigem());
+                if (!trechos.containsKey(encomenda.getOrigem())) {
+                    System.out.println(String.format("MapaDeTrechos não contém o vértice de origem: \"%s\"\n", encomenda.getOrigem()));
+                } else {
+                    FileUtils.escreverArquivoRotas(trechos.get(encomenda.getDestino()), nomeArquivoRotas);
+                }
             }
         }
-
     }
 
     /**
      * Implementação do Algoritmo de Dijkstra para menor caminho entre pontos
      */
     public Map<String, Cidade> menorCaminhoDijkstra(String nomeOrigem) {
-        //Verificando se o mapa de trechos contém a cidade de origem
-        if (!trechos.containsKey(nomeOrigem)) {
-            System.out.println(String.format("Mapa de trechos não contém a cidade de origem: %s", nomeOrigem));
-            return null;
-        }
-        final Cidade origem = trechos.get(nomeOrigem);
-        NavigableSet<Cidade> cidades = new TreeSet<>();
 
-        // Inicializando as cidades
-        for (Cidade cidade : trechos.values()) {
-            cidade.setAnterior(cidade == origem ? origem : null);
-            cidade.setDistancia(cidade == origem ? 0 : Integer.MAX_VALUE);
-            cidades.add(cidade);
-        }
+        if(!Objects.isNull(nomeOrigem) && verificarExistenciaCidadeOrigem(nomeOrigem)) {
+            final Cidade origem = trechos.get(nomeOrigem);
+            NavigableSet<Cidade> cidades = new TreeSet<>();
 
-        Cidade cidade, vizinho;
-        while (!cidades.isEmpty()) {
-
-            // Cidade com a menor distância
-            cidade = cidades.pollFirst();
-
-            if (cidade.getDistancia() == Integer.MAX_VALUE) {
-                break;
+            // Inicializando as cidades
+            for (Cidade cidade : trechos.values()) {
+                cidade.setAnterior(cidade == origem ? origem : null);
+                cidade.setDistancia(cidade == origem ? 0 : Integer.MAX_VALUE);
+                cidades.add(cidade);
             }
 
-            // Verificando as distâncias entre cada cidade vizinha
-            for (Map.Entry<Cidade, Integer> a : cidade.getVizinhos().entrySet()) {
+            Cidade cidade, vizinho;
+            while (!cidades.isEmpty()) {
 
-                // O vizinho dessa iteração
-                vizinho = a.getKey();
+                // Cidade com a menor distância
+                cidade = cidades.pollFirst();
 
-                final int distAlternativa = cidade.getDistancia() + a.getValue();
-                if (distAlternativa < vizinho.getDistancia()) {
-                    // Menor caminho encontrado
-                    cidades.remove(vizinho);
-                    vizinho.setDistancia(distAlternativa);
-                    vizinho.setAnterior(cidade);
-                    cidades.add(vizinho);
+                if (cidade.getDistancia() == Integer.MAX_VALUE) {
+                    break;
+                }
+
+                // Verificando as distâncias entre cada cidade vizinha
+                for (Map.Entry<Cidade, Integer> a : cidade.getVizinhos().entrySet()) {
+
+                    // O vizinho dessa iteração
+                    vizinho = a.getKey();
+
+                    final int distAlternativa = cidade.getDistancia() + a.getValue();
+                    if (distAlternativa < vizinho.getDistancia()) {
+                        // Menor caminho encontrado
+                        cidades.remove(vizinho);
+                        vizinho.setDistancia(distAlternativa);
+                        vizinho.setAnterior(cidade);
+                        cidades.add(vizinho);
+                    }
                 }
             }
         }
 
         return trechos;
+    }
+
+    private boolean verificarExistenciaCidadeOrigem(String nomeOrigem) {
+        if (!trechos.containsKey(nomeOrigem)) {
+            System.out.println(String.format("Mapa de trechos não contém a cidade de origem: %s", nomeOrigem));
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -4,44 +4,43 @@ import domain.Encomenda;
 import domain.Cidade;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class FileUtils {
 
     private static final String DELIMITADOR = " ";
 
     public static Map<String, Cidade> lerArquivoTrechos(String nomeArquivo) {
-        String linha;
-        int numLinha = 1;
+
         Map<String, Cidade> arestas = new HashMap<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(String.format("src/main/resources/%s.txt", nomeArquivo)))) {
-            while ((linha = br.readLine()) != null) {
-                String[] lineArray = linha.split(DELIMITADOR);
+        if (!Objects.isNull(nomeArquivo)) {
+            String linha;
+            int numLinha = 1;
 
-                if (lineArray.length == 3) {
-                    if (!arestas.containsKey(lineArray[0])) {
-                        arestas.put(lineArray[0], new Cidade(lineArray[0]));
+            try (BufferedReader br = new BufferedReader(new FileReader(String.format("src/main/resources/%s.txt", nomeArquivo)))) {
+                while ((linha = br.readLine()) != null) {
+                    String[] lineArray = linha.split(DELIMITADOR);
+
+                    if (lineArray.length == 3) {
+                        if (!arestas.containsKey(lineArray[0])) {
+                            arestas.put(lineArray[0], new Cidade(lineArray[0]));
+                        }
+                        if (!arestas.containsKey(lineArray[1])) {
+                            arestas.put(lineArray[1], new Cidade(lineArray[1]));
+                        }
+                        arestas.get(lineArray[0]).addVizinho(arestas.get(lineArray[1]), Integer.parseInt(lineArray[2]));
+                        numLinha++;
+                    } else {
+                        System.out.println(String.format("A linha %d do arquivo trechos.txt foi pulada porque está no formato errado!", numLinha));
+                        numLinha++;
                     }
-                    if (!arestas.containsKey(lineArray[1])) {
-                        arestas.put(lineArray[1], new Cidade(lineArray[1]));
-                    }
-                    arestas.get(lineArray[0]).addVizinho(arestas.get(lineArray[1]), Integer.parseInt(lineArray[2]));
-                    numLinha++;
-                } else {
-                    System.out.println(String.format("A linha %d do arquivo trechos.txt foi pulada porque está no formato errado!", numLinha));
-                    numLinha++;
                 }
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-
         return arestas;
     }
 
@@ -49,60 +48,70 @@ public class FileUtils {
 
         List<Encomenda> encomendas = new ArrayList<>();
 
-        String linha;
-        int numLinha = 1;
+        if (!Objects.isNull(nomeArquivo)) {
 
-        try (BufferedReader br = new BufferedReader(new FileReader(String.format("src/main/resources/%s.txt", nomeArquivo)))) {
-            while ((linha = br.readLine()) != null) {
-                String[] vetorLinha = linha.split(DELIMITADOR);
+            String linha;
+            int numLinha = 1;
 
-                if (vetorLinha.length == 2) {
-                    Encomenda encomenda = new Encomenda(vetorLinha[0], vetorLinha[1]);
-                    encomendas.add(encomenda);
-                } else {
-                    System.out.println(String.format("A linha %d do arquivo %s.txt foi pulada porque está no formato errado!", numLinha, nomeArquivo));
-                    numLinha++;
+            try (BufferedReader br = new BufferedReader(new FileReader(String.format("src/main/resources/%s.txt", nomeArquivo)))) {
+                while ((linha = br.readLine()) != null) {
+                    String[] vetorLinha = linha.split(DELIMITADOR);
+
+                    if (vetorLinha.length == 2) {
+                        Encomenda encomenda = new Encomenda(vetorLinha[0], vetorLinha[1]);
+                        encomendas.add(encomenda);
+                    } else {
+                        System.out.println(String.format("A linha %d do arquivo %s.txt foi pulada porque está no formato errado!", numLinha, nomeArquivo));
+                        numLinha++;
+                    }
+
                 }
 
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         return encomendas;
     }
 
     public static void escreverArquivoRotas(Cidade cidade, String nomeArquivo) {
-        File log = new File(String.format("src/main/resources/%s.txt", nomeArquivo));
-        try{
-            if(!log.exists()){
-                System.out.println("O arquivo de rotas foi criado!");
-                log.createNewFile();
+
+        if (!Objects.isNull(cidade) && !Objects.isNull(nomeArquivo)) {
+            File log = new File(String.format("src/main/resources/%s.txt", nomeArquivo));
+            try {
+                if (!log.exists()) {
+                    System.out.println("O arquivo de rotas foi criado!");
+                    log.createNewFile();
+                }
+                PrintWriter out = new PrintWriter(new FileWriter(log, true));
+                boolean success = imprimirRota(out, cidade);
+                if (success) {
+                    out.append(Integer.toString(cidade.getDistancia()));
+                }
+                out.append("\n");
+                out.close();
+            } catch (IOException e) {
+                System.out.println("Não foi possível criar o arquivo de rotas!");
             }
-            PrintWriter out = new PrintWriter(new FileWriter(log, true));
-            boolean success = imprimirRota(out, cidade);
-            if (success) {
-                out.append(Integer.toString(cidade.getDistancia()));
-            }
-            out.append("\n");
-            out.close();
-        }catch(IOException e){
-            System.out.println("Não foi possível criar o arquivo de rotas!");
         }
     }
 
     private static boolean imprimirRota(PrintWriter out, Cidade cidade) {
-        if (cidade.getAnterior() == cidade) {
-            out.append(String.format("%s ", cidade.getNome()));
-            return true;
-        } else if (cidade.getAnterior() == null) {
-            out.append(String.format("O vértice %s não é alcançável!", cidade.getNome()));
-            return false;
+        if (!Objects.isNull(out)) {
+            if (cidade.getAnterior() == cidade) {
+                out.append(String.format("%s ", cidade.getNome()));
+                return true;
+            } else if (cidade.getAnterior() == null) {
+                out.append(String.format("O vértice %s não é alcançável!", cidade.getNome()));
+                return false;
+            } else {
+                imprimirRota(out, cidade.getAnterior());
+                out.append(String.format("%s ", cidade.getNome()));
+                return true;
+            }
         } else {
-            imprimirRota(out, cidade.getAnterior());
-            out.append(String.format("%s ", cidade.getNome()));
-            return true;
+            return false;
         }
     }
 }
